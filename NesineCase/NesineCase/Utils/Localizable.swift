149f51097568001745d8aa43ddeaee2bb9fde005
cankurtur <cankurtur@gmail.com>
2023-05-13 03:39:05 +0300

    create localizable

---
+++ NesineCase/NesineCase/Utils/Localizable.swift
@@ -0,0 +1,21 @@
+//
+//  Localizable.swift
+//  NesineCase
+//
+//  Created by Can Kurtur on 13.05.2023.
+//
+
+
+import UIKit
+
+struct Localizable {
+    
+    // MARK: - Home
+    
+    static let searchBarPlaceholder = NSLocalizedString("searchBar.placeholder", comment: "")
+    static let imageSizeLowSectionTitle = NSLocalizedString("imageSizeSectionTitle.low", comment: "")
+    static let imageSizeNormalSectionTitle = NSLocalizedString("imageSizeSectionTitle.normal", comment: "")
+    static let imageSizeHighSectionTitle = NSLocalizedString("imageSizeSectionTitle.high", comment: "")
+    static let imageSizeSuperHighSectionTitle = NSLocalizedString("imageSizeSectionTitle.superHigh", comment: "")
+
+}
