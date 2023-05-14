ac1f37d1c0d352f5f5408ca7da8385035ad036bd
cankurtur <cankurtur@gmail.com>
2023-05-14 18:26:25 +0300

    add new localizables

--- NesineCase/NesineCase/Utils/Localizable.swift
+++ NesineCase/NesineCase/Utils/Localizable.swift
@@ -17,5 +17,12 @@ struct Localizable {
     static let imageSizeNormalSectionTitle = NSLocalizedString("imageSizeSectionTitle.normal", comment: "")
     static let imageSizeHighSectionTitle = NSLocalizedString("imageSizeSectionTitle.high", comment: "")
     static let imageSizeSuperHighSectionTitle = NSLocalizedString("imageSizeSectionTitle.superHigh", comment: "")
+    static let emptyLabelThereIsNoData = NSLocalizedString("emptyLabel.thereIsNoData", comment: "")
 
+
+    // MARK: - General
+    
+    static let generalOk = NSLocalizedString("general.ok", comment: "")
+    static let generalErrorTitle = NSLocalizedString("general.errorTitle", comment: "")
+    static let generalDownloadFailed = NSLocalizedString(" general.downloadFailed", comment: "")
 }
